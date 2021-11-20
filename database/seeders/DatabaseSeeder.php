3cbc150a1279cd736f3f5332c8a187678386a653
azamkhon <azamkhon.kh@gmail.com>
2021-11-21 00:34:48 +0500

    seeder check

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-
         $this->call([
-            RolesSeeder::class
+            RolesSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
