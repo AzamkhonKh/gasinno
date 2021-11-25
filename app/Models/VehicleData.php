2a7cb99cab5171ae2dba86f7f90633ba868cd796
azamkhon <azamkhon.kh@gmail.com>
2021-11-26 02:39:56 +0500

    get method available gps data

--- app/Models/VehicleData.php
+++ app/Models/VehicleData.php
@@ -28,7 +28,7 @@ class VehicleData extends Model
         return $this->belongsTo(User::class,'id','owner_id');
     }
     public function geo(){
-        return $this->hasMany(GISdata::class,'id','vehicle_id');
+        return $this->hasMany(GISdata::class,'vehicle_id','id');
     }
 
 
